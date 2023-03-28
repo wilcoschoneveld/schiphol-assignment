7e1470241a8f8502f48e7e7b361b4167e6e1bdb3
Wilco Schoneveld <schoneveld.wj@gmail.com>
2023-03-28 10:47:39 +0200

    Added simple express API

---
+++ server/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import fs from "fs";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/flights", async (req, res) => {
+  const { airport, limit, order_by } = req.query;
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const rawData = fs.readFileSync("flights.json");
+
+  let flights = JSON.parse(rawData)["flights"];
+
+  if (airport) {
+    flights = flights.filter((flight) =>
+      flight.airport.toLowerCase().includes(airport.toLowerCase())
+    );
+  }
+
+  if (order_by) {
+    const [order_term, dir] = order_by.split(":");
+
+    if (order_term === "date") {
+      flights.sort((a, b) => {
+        const dateA = new Date(a.date + "T" + a.expectedTime);
+        const dateB = new Date(b.date + "T" + b.expectedTime);
+        return (dateA - dateB) * (dir === "desc" ? -1 : 1);
+      });
+    }
+  }
+
+  if (limit) {
+    flights = flights.slice(0, Number(limit));
+  }
+
+  res.json({ flights: flights });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
